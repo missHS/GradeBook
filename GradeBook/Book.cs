9414fe838bd0d40ea2492f3d2203f21bb08fad26
missHS <helene.schonbeck@gmail.com>
2021-01-19 16:50:03 +0100

    Add project files.

---
+++ GradeBook/Book.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GradeBook
+{
+    class Book
+    {
+        private string name;
+        private List<double> grades = new List<double>();
+
+       
+        public Book(string name)
+        {
+            this.name = name;
+        }
+        public void AddGrade(double grade)
+        {
+            grades.Add(grade);
+        }
+
+        
+
+        public void ShowStatistics()
+        {
+            var result = 0.0;
+            var highGrade = double.MinValue;
+            var lowGrade = double.MaxValue;
+
+            foreach (var number in grades)
+            {
+                if (number > highGrade)
+                {
+                    highGrade = number;
+                }
+                else if (number < lowGrade)
+                {
+                    lowGrade = number;
+                }
+                result += number;
+            }
+
+            var avarage = result / grades.Count;
+
+            Console.WriteLine($"The avarage grade is {avarage:N1}");
+            Console.WriteLine($"The highets grade is {highGrade}");
+            Console.WriteLine($"The lowest grade is {lowGrade}");
+
+
+        }
+     
+
+
+    }
+}
+
---
+++ GradeBook/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace GradeBook
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var book = new Book("Helenes book");
+
+            book.AddGrade(54.1);
+            book.AddGrade(98.1);
+            book.AddGrade(12.7);
+            book.AddGrade(5.68);
+
+
+            book.ShowStatistics();
+
+
+
+
+         }
+    }
+}
